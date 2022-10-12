a63b6ab47268839128416fb0d95849b5d66a25ee
eg-delacruz <netoxas3107@hotmail.com>
2022-10-12 13:52:34 +0200

    Bug fix attempt 4

--- services/cron-jobs/send_email_to_incomplete_verif_users.js
+++ services/cron-jobs/send_email_to_incomplete_verif_users.js
@@ -5,7 +5,7 @@ const send_email_to_incomplete_verif_users = new CronJob(
   //'30 14 * * *', //Every day at 2:30 pm
   '* * * * *',
   function () {
-    console.log('You will see this message every second');
+    console.log('You will see this message every second...');
   }
 );
 
