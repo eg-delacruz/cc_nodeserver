6645a114218db50fff56849d5fc46260cc546748
eg-delacruz <netoxas3107@hotmail.com>
2022-10-12 15:27:59 +0200

    Cron Job frequency changed again

--- components/admin/clientes/contract/controller.js
+++ components/admin/clientes/contract/controller.js
@@ -1,31 +1,5 @@
 const store = require('./store');
 
-const generateContract = async ({ cliente, campana, contrato }) => {
-  if (
-    !(
-      cliente.nombre ||
-      cliente.tipo ||
-      cliente.dni ||
-      campana.tipo_de_campana ||
-      contrato.lugar_de_creacion
-    )
-  ) {
-    throw new Error(
-      '[Client/contract controller] Información insuficiente para generar contrato'
-    );
-  }
+const functionName = async () => {};
 
-  const contract_info = {
-    client_name: cliente.nombre.toLowerCase(),
-    client_type: cliente.tipo.toLowerCase(),
-    client_DNI: cliente.dni.toLowerCase(),
-    company: cliente.empresa_representada.toLowerCase(),
-    campaign_type: campana.tipo_de_campana.toLowerCase(),
-    creation_place: contrato.lugar_de_creacion.toLowerCase(),
-    creation_date: new Date(),
-  };
-
-  const created_contract = await store.add(contract_info);
-};
-
-module.exports = { generateContract };
+module.exports = { functionName };
--- components/admin/clientes/contract/network.js
+++ components/admin/clientes/contract/network.js
@@ -13,14 +13,8 @@ const controller = require('./controller.js');
 //Securing route with secret key
 const SECRET_KEY = process.env.SERVER_SECRET_KEY;
 
-//PDF Contract - Generation and fetching of the data
 router.post('/', async function (req, res) {
   if (req.headers.secret_key === SECRET_KEY) {
-    // const cliente = req.body.cliente;
-    // const campana = req.body.campana;
-    // const contrato = req.body.contrato;
-
-    await controller.generateContract(req.body);
     return successResponse(req, res, 'Todo OK', 201);
   } else {
     errorResponse(req, res, 'Forbidden', 403, 'Access denied');
