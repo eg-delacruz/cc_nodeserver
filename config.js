e01bcf58fd4fc440a5a9b187c877c86d88d294e5
eg-delacruz <netoxas3107@hotmail.com>
2022-10-13 09:39:06 +0200

    Changed to main DB to store unfinished verified accounts.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASSWORD;
-const db_name = process.env.DB_TESTING_NAME;
+const db_name = process.env.DB_NAME;
 
 const url = `mongodb+srv://${db_user}:${db_pass}@cluster0.9cjba.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
